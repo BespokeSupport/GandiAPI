e02ab3ef6f16876e13599f258f06fd5ef47c33c6
Richard Seymour <web@bespoke.support>
2015-05-05 08:30:41 +0100

    minor change - require -> include

--- examples/_bootstrap.php
+++ examples/_bootstrap.php
@@ -4,5 +4,5 @@ if (!file_exists(dirname(__FILE__).'/../credentials.php')) {
     throw new Exception('credentials.php file required in root');
 }
 
-require_once dirname(__FILE__).'/../vendor/autoload.php';
+include_once dirname(__FILE__).'/../vendor/autoload.php';
 require_once dirname(__FILE__).'/../credentials.php';
